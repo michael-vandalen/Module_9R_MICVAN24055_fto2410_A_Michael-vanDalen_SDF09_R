a0f604892f6107773b4e280c870b9c18544be58e
Michael van Dalen <michaelvdalen191@gmail.com>
2024-11-19 16:28:06 +0200

    Added the starting JS and CSS

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
+let firstCard = 6;
+let secondCard = 9;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-let sum = firstCard + secondCard
+let sum = firstCard + secondCard + 6;
 
-console.log(sum)
+if (sum <= 20) {
+  message = "Do you want to draw a new card? 😊";
+} else if (sum === 21) {
+  message = "Wohoo! You've got Blackjack! 🥳";
+  hasBlackJack = true;
+} else {
+  message = "You're out of the game! 😭";
+  isAlive = false;
+}
+
+// Cash Out!
+console.log(message);
