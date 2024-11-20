3b4c67e354a48d91ec1f36a7da54938aeadeed2f
Michael van Dalen <michaelvdalen191@gmail.com>
2024-11-20 19:17:37 +0200

    Added the button with a function and editted more styles

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-let sum = firstCard + secondCard + 6;
+let sum = firstCard + secondCard + 4;
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card? 😊";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack! 🥳";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game! 😭";
-  isAlive = false;
+function startGame() {
+  if (sum <= 20) {
+    message = "Do you want to draw a new card? 😊";
+  } else if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  console.log(message);
 }
-
-// Cash Out!
-console.log(message);
